5071e8a01c99845ec1f31e0b1ea73f848b3af30f
Evgenii.Korchagov <joedraiser@gmail.com>
2022-01-24 09:54:48 +0300

    amendments done

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 
 std::mutex pace_record_access;
+std::mutex coutMutex;
 
 struct swimmer
 {
@@ -34,12 +35,16 @@ void swim(std::string name, int pace)
     int time=1;
     while(pace*time<100)
     {
+        coutMutex.lock();
         std:: cout << "Swimmer " << name << " swam " << pace*time << " meters\n";
+        coutMutex.unlock();
         std::this_thread::sleep_for(std::chrono::seconds(1));
         time++;
     }
 
+    coutMutex.lock();
     std:: cout << "Swimmer " << name << " finished with " << 100.0/pace << " seconds\n";
+    coutMutex.unlock();
     pace_record_access.lock();
     race_results.push_back(swimmer (name, 100.0/pace));
     pace_record_access.unlock();
@@ -57,15 +62,21 @@ int main()
         std::cin >> athletes[i].pace;
     }
 
-    for(int i=0;i<6;i++)
-    {
-        std::thread (swim, athletes[i].name, athletes[i].pace).detach();
-    }
+    std::thread swimmer[6];
 
-    while(race_results.size()!=6)
-    {
-        continue;
-    }
+    std::thread swimmer1(swim, athletes[0].name, athletes[0].pace);
+    std::thread swimmer2(swim, athletes[1].name, athletes[1].pace);
+    std::thread swimmer3(swim, athletes[2].name, athletes[2].pace);
+    std::thread swimmer4(swim, athletes[3].name, athletes[3].pace);
+    std::thread swimmer5(swim, athletes[4].name, athletes[4].pace);
+    std::thread swimmer6(swim, athletes[5].name, athletes[5].pace);
+
+    swimmer1.join();
+    swimmer2.join();
+    swimmer3.join();
+    swimmer4.join();
+    swimmer5.join();
+    swimmer6.join();
 
     std::sort(race_results.begin(), race_results.end(), race_result_compare);
 
